a34ac6ce568d94ec2656e740d9fca8a266896dc5
Faritor·Kang <32325325+FaritorKang@users.noreply.github.com>
2018-03-19 21:53:17 +0800

    Create caddy_install.sh

---
+++ caddy_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#author faritor@unmz.net
+#date 2018-3-19 21:47:34
+#description Simplify the installation process only need to configure Caddyfile can use
+
+
+curl https://getcaddy.com | bash -s personal
+
+mkdir /etc/caddy
+
+mv /usr/local/bin/caddy /etc/caddy/
+
+echo >> /etc/caddy/Caddyfile
+
+echo "#!/bin/bash \n\n\n\nnohup ./caddy -conf="./Caddyfile" >/dev/null 2>log & " >> /etc/caddy/start
+
+echo "#!/bin/bash \n\n\n\npkill caddy" >> /etc/caddy/stop
+
+chmod +x /etc/caddy/s*
