f4bcff033b960c9169ee4306ff8205341350b736
Faritor <32325325+FaritorKang@users.noreply.github.com>
2019-11-23 14:35:44 +0800

    Update createSSL.sh

--- createSSL.sh
+++ createSSL.sh
@@ -4,7 +4,7 @@
 #	Email: faritor@unmz.net
 #	Date: 2019-11-22 20:53:09
 #
-#######################
+#####################################################################
 
 read -p "请输入需要生成证书的域名:" DOMAIN
 if [ -z "$DOMAIN" ];then
@@ -26,6 +26,14 @@ if [ ! -d "~/.acme.sh" ];then
     echo -e "\033[36m本机或当前登录用户还未安装acme,正在下载...\033[0m"
     curl https://get.acme.sh | sh
     echo -e "\033[36m下载完成,开始创建证书\033[0m"
+fi
+
+if [ ! -d "$WWWROOT" ];then
+	mkdir $WWWROOT
+fi
+
+if [ ! -d "$SSL_PATH" ];then
+	mkdir $SSL_PATH
 fi
 
 PWDIR=$WWWROOT/$DOMAIN
